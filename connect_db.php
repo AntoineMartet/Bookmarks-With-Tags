6f7a2fb0ab743d9d2c6efc22d0bad3f15f501266
AntoineMartet <antoine.martet@cpnv.ch>
2023-07-17 16:22:44 +0200

    connection to db fix

--- connect_db.php
+++ connect_db.php
@@ -5,3 +5,20 @@
  * @author   Created by AntoineM
  * @version  16.07.2023
  */
+
+$dsn = "mysql:host=bwt-database.c1vsmfbbxgh8.eu-west-3.rds.amazonaws.com;dbname=bwt-database"; // Data Source Name
+$dbPort = "3306";
+$dbUsername = "Admin";
+$dbPwd = "KuLo670zH7s8";
+
+try{
+    $pdo = new PDO($dsn, $dbUsername, $dbPwd);
+    $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+}catch(PDOException $e){
+    echo "Connection failed: " . $e->getMessage();
+}
+
+// SQL pour récupération de la table des users
+$usersRequest = $pdo->query('SELECT * FROM users');
+echo $usersRequest;
+
--- index.php
+++ index.php
@@ -5,3 +5,26 @@
  * @author   Created by AntoineM
  * @version  14.07.2023
  */
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <meta charset="UTF-8">
+    <title>Login</title>
+    <link rel="stylesheet" href="css/style.css">
+    <script src="https://code.jquery.com/jquery-3.6.0.min.js"></script>
+</head>
+<body>
+    <div>
+        <h2>Bienvenue sur Bookmarks With Tags :</h2>
+    </div>
+</body>
+</html>
+
+<?php
+include 'connect_db.php';
+
+
+?>
